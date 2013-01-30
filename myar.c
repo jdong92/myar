769518a15a499fcd89e0f6f609d83f5aa8afde91
John Dong <dongj@os-class.engr.oregonstate.edu>
2013-01-30 00:39:01 -0800

    First Commit

---
+++ myar.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BLOCKSIZE 1024
+
+int main ( int argc, char *argv[])
+{
+	int inputFd;
+	ssize_t numRead;
+	char buffer[BLOCKSIZE + 1];
+	
+	if (argc <= 2) {
+		printf("Usage: %s key afile name ... \n", argv[0]);	
+		
+		}
+
+	if ((argc == 3) && strcmp(argv[1], "-v") == 0){
+		printf("-v option \n");
+		
+		}
+
+	
+
+}
+
+
