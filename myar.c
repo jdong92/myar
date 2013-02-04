40032088bcaf82c72b045cdcfc4d5a912f1c7d54
John Dong <jdong1992@gmail.com>
2013-02-03 16:50:04 -0800

    Working on the extraction feature

--- myar.c
+++ myar.c
@@ -10,20 +10,22 @@
 #define SARMAG 8
 #define ARMAG "!<arch>\n"
 #define ARFMAG "`\n"
+#define HEADER_SIZE 61
 #define HEADER_READ_SIZE 60
 #define BLOCKSIZE 1024
 
 int main ( int argc, char *argv[])
 {
 
-	int inputFd, archiveFd;
+	int inputFd, archiveFd, numWritten = 0;
 	ssize_t numRead;
-	char buffer[BLOCKSIZE]; /*File buffer*/
+	char buffer[BLOCKSIZE], header[HEADER_SIZE]; /*File buffer*/
 	unsigned long filesize;
 	long date;
 	struct tm timestruct;
 	static char timestr[100];
 	struct stat *fileStat;
+	char name[16];
 	fileStat = (struct stat *)malloc(sizeof(struct stat));
 
 	struct ar_hdr
@@ -147,25 +149,93 @@ int main ( int argc, char *argv[])
 		}
 		
 		char fileBuffer[fileStat->st_size];
-
-		strcpy(ar->ar_name,argv[3]);
-		snprintf(buffer, HEADER_READ_SIZE, "%-16s%-12ld%-6d%-6d%-8o%-10ld\n", ar->ar_name, fileStat->st_mtime, fileStat->st_uid, fileStat->st_gid, fileStat->st_mode, fileStat->st_size);
-
 		lseek(archiveFd, 0, SEEK_END);
-		write(archiveFd, buffer, HEADER_READ_SIZE);
+
+		int len = strlen(argv[3]);
+		memset(name, 0, 16);
+		int i = 0;
+		for (i = 0; i < 16; i++){
+				if (argv[3][i] == '\0'){
+					name[i] = '/';
+					break;
+				}else{
+				name[i] = argv[3][i];
+				}
+			}
+
+		snprintf(header, HEADER_SIZE, "%-16s%-12ld%-6d%-6d%-8o%-10ld%-2s", name, fileStat->st_mtime, fileStat->st_uid, fileStat->st_gid, fileStat->st_mode, fileStat->st_size,ARFMAG);
+
+			numWritten = write(archiveFd, header, HEADER_SIZE);
+
+		if (numWritten == -1){
+
+			perror("Error writing file header.");
+			unlink(argv[2]);
+			exit(-1);
+		}
 	
 		lseek(archiveFd, -1, SEEK_CUR);
 
-		read(inputFd, fileBuffer, fileStat->st_size);
-		numRead = write(archiveFd, fileBuffer, fileStat->st_size);		
+		while ((numRead = read(inputFd, fileBuffer, BLOCKSIZE)) > 0) {
 
+			numWritten = write(archiveFd, fileBuffer, numRead);
+
+		}
+	
 		if (inputFd == -1){
 			exit(-1);
 		}
 
-
+		close(inputFd);
+		close(archiveFd);
 
 	}/* End Q OPTION */
+
+	if ((argc == 4) && strcmp(argv[1], "x") == 0){
+		
+		inputFd = open(argv[2], O_RDONLY);
+
+		if (inputFd == -1){
+			perror("Error creating file.");
+			exit(-1);
+		}
+
+		numRead = read(inputFd, buffer, SARMAG);
+		if (strncmp(buffer, ARMAG, SARMAG) != 0){
+			perror("Unknown archive file \n");
+			exit(-1);
+		}
+
+		while((numRead = read(inputFd, buffer, HEADER_READ_SIZE)) == HEADER_READ_SIZE)
+		{
+			sscanf(buffer, "%s %s %s %s %s %s %s", ar->ar_name, ar->ar_date, ar->ar_uid, ar->ar_gid,ar->ar_mode, ar->ar_size, ar->ar_fmag);
+			memset(name, 0, 16);
+
+			int i = 0;
+			while(ar->ar_name[i] != '/'){
+
+				name[i] = ar->ar_name[i];
+				i++;
+
+			}
+
+			filesize = strtoul(ar->ar_size, NULL, 10);
+
+			if (strcmp(name, argv[3]) == 0){
+				
+				printf("File Exists\n");	
+			}
+
+			
+			lseek(inputFd, filesize, SEEK_CUR);
+
+			if (filesize % 2 != 0){
+				lseek(inputFd, 1, SEEK_CUR);
+			}
+
+		}
+
+	}/* End X OPTION */
 
 
 }/* End Main */
